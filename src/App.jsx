1c1c9a3d8a54bb025c7b538afe0aae6796df8da6
Hassan-1453 <hs002941@gmail.com>
2025-09-19 01:49:46 +0500

    the test is complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 
 const App = () => {
   return (
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+<>
+<p>hello</p>
+
+<h1>
+  hello hassan post job
+</h1>
+
+<p>
+  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id atque, excepturi laudantium quidem, consectetur incidunt quasi quibusdam fugit veniam, minus possimus iusto animi aliquam numquam qui error corporis nesciunt tempora!
+</p>
+</>
   )
 }
 
