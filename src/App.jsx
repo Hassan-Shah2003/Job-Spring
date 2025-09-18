3a2564a9b0ead45dbb61080fb5313b64acaf1633
Hassan-1453 <hs002941@gmail.com>
2025-09-19 02:06:51 +0500

    Post job page (#2)
    * testing sql
    
    * testing sql
    
    * testing sql
    
    * testing sql
    
    * Update App.jsx
    
    * testing branch of post job
    
    * again testing
    
    * testing

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,7 @@ import React from 'react'
 const App = () => {
   return (
 <>
-<p>hello</p>
-
-<h1>
-  hello hassan post job
-</h1>
-
-<p>
-  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id atque, excepturi laudantium quidem, consectetur incidunt quasi quibusdam fugit veniam, minus possimus iusto animi aliquam numquam qui error corporis nesciunt tempora!
-</p>
+<h1>hello testing</h1>
 </>
   )
 }
