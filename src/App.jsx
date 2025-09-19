f960a1bd6aa854789a980f81b55e70f0c1cf25ce
Hassan-1453 <hs002941@gmail.com>
2025-09-19 19:52:10 +0500

    Post job page (#7)
    * testing sql
    
    * testing sql
    
    * testing sql
    
    * testing sql
    
    * Update App.jsx
    
    * testing branch of post job
    
    * again testing
    
    * testing
    
    * testing
    
    * final testing for post job
    
    * final testing from post job

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 const App = () => {
   return (
 <>
+<h4>hello from post job</h4>
+<span>hello form post job</span>
 <h3>hello form devekop</h3>
 <p>hello from deve</p>
 </>
