bc361a4f8d3da78332baaad710db5a9d5cf24ed6
Hassan-Shah2003 <hs002941@gmail.com>
2025-09-20 11:39:09 +0500

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,7 @@ import React from 'react'
 const App = () => {
   return (
 <>
-<h4>hello from post job</h4>
-<span>hello form post job</span>
-<h3>hello form devekop</h3>
-<p>hello from deve</p>
+    <h1>hello</h1>
 </>
   )
 }
